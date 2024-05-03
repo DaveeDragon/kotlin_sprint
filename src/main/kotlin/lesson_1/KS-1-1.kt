9cf98622a634150965a25424ee62724f2d811d4b
davee_dragon <dave.haris06@gmail.com>
2024-05-03 23:06:01 +0300

    KS-1-1 create variables

---
+++ src/main/kotlin/lesson_1/KS-1-1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_1
+
+fun main(){
+    val numbersOfOrders = 75
+    val thanksForBusiness = "Спасибо большое за покупку, надеемся вам понравится товарБ ждем вас еще!"
+    println(numbersOfOrders)
+    println(thanksForBusiness)
+}
