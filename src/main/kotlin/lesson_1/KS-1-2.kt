83e03b89f20c8ef465e7b6cbbddb3d3e0fb3a5d4
davee_dragon <dave.haris06@gmail.com>
2024-05-06 10:13:05 +0300

    KS-1-2 update variebles

---
+++ src/main/kotlin/lesson_1/KS-1-2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main(){
+    val numbersOfOrders = 75
+    val thanksForBusiness = "Спасибо большое за покупку, надеемся вам понравится товарБ ждем вас еще!"
+    //один работник уволился
+   // val numberOfEmployees = 2000
+    val numberOfEmployees = 1999
+
+    println(numbersOfOrders)
+    println(thanksForBusiness)
+    println(numberOfEmployees)
+}
