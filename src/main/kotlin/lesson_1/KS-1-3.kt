01e068b4762a7729d0580088cc932d47803b8fab
davee_dragon <dave.haris06@gmail.com>
2024-05-06 11:32:24 +0300

    KS-1-3 Gagarin flight

---
+++ src/main/kotlin/lesson_1/KS-1-3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    hour = 10
+    minute = 55
+
+
+    println(year)
+    println(hour)
+    println(minute)
+    println("${hour}:${minute}")
+
+}
