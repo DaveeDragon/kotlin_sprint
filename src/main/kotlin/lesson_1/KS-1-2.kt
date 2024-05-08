266f3c705f46afecbc71fde275fab1db5fa21ce5
davee_dragon <dave.haris06@gmail.com>
2024-05-08 14:00:24 +0300

    KS-1-2 refactored

--- src/main/kotlin/lesson_1/KS-1-2.kt
+++ src/main/kotlin/lesson_1/KS-1-2.kt
@@ -4,8 +4,9 @@ fun main(){
     val numbersOfOrders = 75
     val thanksForBusiness = "Спасибо большое за покупку, надеемся вам понравится товарБ ждем вас еще!"
     //один работник уволился
-   // val numberOfEmployees = 2000
-    val numberOfEmployees = 1999
+    var numberOfEmployees = 2000
+    //странно что считает инициализацию грубой
+    numberOfEmployees = 1999
 
     println(numbersOfOrders)
     println(thanksForBusiness)
