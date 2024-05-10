1ee1fbdd2b2be41fc94f30b1bcb9f415acb454d2
davee_dragon <dave.haris06@gmail.com>
2024-05-10 16:08:59 +0300

    KS-4-1

---
+++ src/main/kotlin/lesson_4/KS-4-1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_4
+
+fun main() {
+    val busyTablesToday = 13
+    val busyTablesTommorow = 9
+
+    println("Доступность столиков на сегодня: ${busyTablesToday != 13}")
+    println("Доступность столиков на сегодня: ${busyTablesTommorow != 13}")
+}
