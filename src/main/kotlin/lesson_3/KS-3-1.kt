8d3d518d383309841fefb692ee31ee82fbbd4b50
davee_dragon <dave.haris06@gmail.com>
2024-05-10 16:07:15 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson_3/KS-3-1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main(){
+    val userName = "Давид"
+    val greeting = "Добрый"
+
+    println("$greeting день, $userName")
+    println("$greeting вечер, $userName")
+}
