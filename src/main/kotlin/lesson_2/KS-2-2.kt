e135b339fa4b287b3e816aae538a28b614ee2980
davee_dragon <dave.haris06@gmail.com>
2024-05-10 16:04:09 +0300

    KS-2-2

---
+++ src/main/kotlin/lesson_2/KS-2-2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main(){
+
+    val salariesOfPermanentEmployees = 50 * 30000
+    val generalExpenaes = salariesOfPermanentEmployees + 30 * 20000
+    val averageSalary = generalExpenaes/80
+
+    println(salariesOfPermanentEmployees)
+    println(generalExpenaes)
+    println(averageSalary)
+
+
+}
