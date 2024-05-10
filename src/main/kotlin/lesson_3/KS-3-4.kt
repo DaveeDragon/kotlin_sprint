c7f9f9d09b7afaa4173b53735095bde5b94e828e
davee_dragon <dave.haris06@gmail.com>
2024-05-10 16:08:21 +0300

    KS-3-4

---
+++ src/main/kotlin/lesson_3/KS-3-4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main(){
+    var fromWhere = "E2"
+    var toWhere = "E4"
+    var moveNumber = 1
+    println("$fromWhere-$toWhere; $moveNumber")
+
+    fromWhere = "D2"
+    toWhere = "D3"
+    moveNumber = 2
+    println("$fromWhere-$toWhere; $moveNumber")
+
+}
