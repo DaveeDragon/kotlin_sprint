e492853433737acbe539db9bc2457e1d2d1ed921
davee_dragon <dave.haris06@gmail.com>
2024-05-10 16:07:36 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/KS-3-2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main(){
+    val fioBeforeMarriege = "Андреева Татьяна Сергеевна"
+    val ageBeforeMarriege = 20
+
+    val fioAfterMarriege = "Сидорова Татьяна Сергеевна"
+    val ageAfterMarriege = 22
+
+    println("$fioBeforeMarriege, $ageBeforeMarriege")
+    println("$fioAfterMarriege, $ageAfterMarriege")
+}
