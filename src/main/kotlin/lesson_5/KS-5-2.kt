8db43761afe29b123db4c3db5e53107dc767c981
davee_dragon <dave.haris06@gmail.com>
2024-05-15 20:31:54 +0300

    KS-5-2

---
+++ src/main/kotlin/lesson_5/KS-5-2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_5
+
+fun main(){2
+    println("введите год рождения")
+    val ageOfBirth = readLine()!!.toInt()
+    val age = 2024 - ageOfBirth
+    if (age>=18){
+        println("Показать экран со скрытым контентом")
+    }
+    else{
+        println("Доступ запрещен")
+    }
+}
