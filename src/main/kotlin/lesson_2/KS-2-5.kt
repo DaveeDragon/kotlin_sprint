525120d5d71b6ce00a05a6c7dc74fcbd64ff03c7
davee_dragon <dave.haris06@gmail.com>
2024-05-21 03:54:25 +0300

    KS-2-5 resolved

--- src/main/kotlin/lesson_2/KS-2-5.kt
+++ src/main/kotlin/lesson_2/KS-2-5.kt
@@ -3,8 +3,10 @@ package org.example.lesson_2
 import kotlin.math.pow
 
 fun main(){
-    val contribution = 70000.000
-    val contributionAfterTwentyYears = contribution*(1.167.pow(20))
+    val interestRate = 0.167
+    val years = 20
+    val contribution = 70000.0
+    val contributionAfterTwentyYears = contribution * ((1 + interestRate).pow(years))
     println(String.format("%.3f",contributionAfterTwentyYears))
 
 }
