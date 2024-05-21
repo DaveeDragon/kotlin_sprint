94290d2d3b2f315847c1ce0ffaae7af1a3faec56
davee_dragon <dave.haris06@gmail.com>
2024-05-21 03:49:56 +0300

    KS-2-4 resolved

--- src/main/kotlin/lesson_2/KS-2-4.kt
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -4,9 +4,10 @@ package org.example.lesson_2
 fun main() {
     val crystalOre: Int = 7
     val ironOre: Int = 11
+    val buff = 0.2
 
-    val buffCrystalOre: Int = (crystalOre * 0.2).toInt()
-    val buffIronOre: Int = (ironOre * 0.2).toInt()
+    val buffCrystalOre: Int = (crystalOre * buff).toInt()
+    val buffIronOre: Int = (ironOre * buff).toInt()
 
     println("бонусная кристалическая руда: $buffCrystalOre")
     println("бонусная железная руда: $buffIronOre")
