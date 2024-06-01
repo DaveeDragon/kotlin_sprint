ca4517684c9854ef47612eaaea3721835a8330bb
david <dave.haris06@gmail.com>
2024-06-01 18:03:38 +0300

    KS-5-1 refactored little bit

--- src/main/kotlin/lesson_5/KS-5-1.kt
+++ src/main/kotlin/lesson_5/KS-5-1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_5
 
 fun main(){
-    println(" Докажите что вы не бот решив5 это простое выражение:   2+2=...")
-    var a = readLine()!!.toInt()
+    println(" Докажите что вы не бот решив это простое выражение:   2+2=...")
+    val a = readLine()!!.toInt()
     println("2+2=$a")
-    if (a==4){
+    if (a == 4){
         println("Добро пожаловать!!!")
     }
     else{
