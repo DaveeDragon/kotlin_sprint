22fba0228a6562f1fbcb1163bf06f42c8aaa1cf2
david <dave.haris06@gmail.com>
2024-06-01 17:56:06 +0300

    KS-4-5 refactored

--- src/main/kotlin/lesson_4/KS-4-5.kt
+++ src/main/kotlin/lesson_4/KS-4-5.kt
@@ -4,7 +4,13 @@ package org.example.lesson_4
      val shipNoInjured = readLine().toBoolean()
      val numberOfCrew: Int = readLine()!!.toInt()
      val quantityOfProvisions: Int = readLine()!!.toInt()
-     val weatherIsFovorable = readLine().toBoolean()
+     val weatherIsFavorable = readLine().toBoolean()
 
-     println("Научно-исследовательский корабль может приступить к долгосрочному плаванию: ${((shipNoInjured)and(numberOfCrew<70)and(numberOfCrew>50)and(quantityOfProvisions>50)and(weatherIsFovorable))||((!shipNoInjured)and(numberOfCrew==70)faand(quantityOfProvisions>50)and(weatherIsFovorable))}")
+     val conditionsNoInjured = true
+     val minCrewNumber = 55
+     val maxCrewNumber = 70
+     val conditionProvisionsQuantity = 50
+     val conditionFavorableWeather = true
+
+     println("Научно-исследовательский корабль может приступить к долгосрочному плаванию: ${(((shipNoInjured == conditionsNoInjured)and(numberOfCrew < maxCrewNumber) and (numberOfCrew > minCrewNumber) and (quantityOfProvisions > conditionProvisionsQuantity)and(weatherIsFavorable == conditionFavorableWeather)) || ((numberOfCrew == maxCrewNumber) and (quantityOfProvisions > conditionProvisionsQuantity) and (weatherIsFavorable == conditionFavorableWeather)))}")
  }
