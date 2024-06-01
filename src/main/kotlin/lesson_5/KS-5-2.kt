802f56e300338cb960e9dc58e7e753d4a98e1ea7
david <dave.haris06@gmail.com>
2024-06-01 18:08:16 +0300

    KS-5-2 refactored a little bit

--- src/main/kotlin/lesson_5/KS-5-2.kt
+++ src/main/kotlin/lesson_5/KS-5-2.kt
@@ -4,7 +4,7 @@ fun main(){2
     println("введите год рождения")
     val ageOfBirth = readLine()!!.toInt()
     val age = 2024 - ageOfBirth
-    if (age>=18){
+    if (age >= 18){
         println("Показать экран со скрытым контентом")
     }
     else{
