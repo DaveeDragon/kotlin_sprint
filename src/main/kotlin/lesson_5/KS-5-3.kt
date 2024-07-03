695911e275927a69430cec494cec8e0f9ab00244
david <dave.haris06@gmail.com>
2024-07-03 23:12:16 +0300

    KS-5-3 refactored

--- src/main/kotlin/lesson_5/KS-5-3.kt
+++ src/main/kotlin/lesson_5/KS-5-3.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_5
 
 fun main() {
-    val a = 5
-    val b = 7
+    val randomNumber1 = 5
+    val randomNumber2 = 7
 
-    println("Вам нужно угадать два числа от 0 до 42, напишите их5")
-    val a1 = readLine()!!.toInt()
-    val b1 = readLine()!!.toInt()
-    if ((a1 == a) and (b1 == b))  println("Поздравляем! Вы выиграли главный приз!")
-    else if ((a1 == a) or (b1 == b))  println("Вы выиграли утешительный приз!")
+    println("Вам нужно угадать два числа от 0 до 42, напишите их")
+    val number1 = readLine()!!.toInt()
+    val number2 = readLine()!!.toInt()
+    if ((randomNumber1 == number1) and (randomNumber2 == number2))  println("Поздравляем! Вы выиграли главный приз!")
+    else if ((randomNumber1 == number1) or (randomNumber2 == number2))  println("Вы выиграли утешительный приз!")
     else println("Неудача!")
 }
