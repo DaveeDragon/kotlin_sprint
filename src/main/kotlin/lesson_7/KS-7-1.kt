691d57f00cef3488b783dc43570a3abb2c37fee3
david <dave.haris06@gmail.com>
2024-07-05 19:32:41 +0300

    KS-7-1 prototype

---
+++ src/main/kotlin/lesson_7/KS-7-1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_7
+
+fun main(){
+     val numberRange = 0..9
+     val stringRange = 'a'..'z'
+    var password = stringRange
+    for (i in 3 downTo 1 )
+    {
+        password =
+    }
+    println(password)
+}
