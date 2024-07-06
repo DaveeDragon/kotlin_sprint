7306b871a88706b502700529dff6bbf9b2bd8274
david <dave.haris06@gmail.com>
2024-07-06 23:18:59 +0300

    KS-7-3 re

--- src/main/kotlin/lesson_7/KS-7-3.kt
+++ src/main/kotlin/lesson_7/KS-7-3.kt
@@ -3,7 +3,7 @@ package org.example.lesson_7
 fun main(){
     println("Напишите число и выведится все четные числа от нуля до этого числа")
     val parityMax = readln().toInt()
-    val parityProgression = 0..parityMax step 2
+     val parityProgression = 0..parityMax step 2
     for (i in parityProgression){
         println(i)
     }
