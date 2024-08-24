86db267856afb3d1e0c32043fd14e32a295e966c
david <dave.haris06@gmail.com>
2024-08-24 04:31:25 +0300

    KS-12-4

---
+++ src/main/kotlin/lesson_12/KS-12-4.kt
@@ -0,0 +1,5 @@
+package org.example.lesson_12
+
+fun main(){
+    val sunday = weather4(298, 289, false)
+}
---
+++ src/main/kotlin/lesson_12/weather4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_12
+
+class weather4(
+    var dayTemperature: Int,
+    var nightTemperature: Int,
+    var isRianing: Boolean,
+)
+{
+    init {
+        println("Дневная температура равна: ${dayTemperature - 273}")
+        println("Ночная температура равна: ${nightTemperature - 273}")
+        println("Идет ли дождь: $isRianing")
+    }
+
+}
