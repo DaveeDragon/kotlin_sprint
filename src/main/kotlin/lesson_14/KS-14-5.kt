7b45f8e958f914d62551c9fc760a753cd3d2dc71
david <dave.haris06@gmail.com>
2024-10-24 05:11:40 +0300

    KS-14-5

--- src/main/kotlin/lesson_14/KS-14-5.kt
+++ src/main/kotlin/lesson_14/KS-14-5.kt
@@ -1,20 +1,37 @@
 package org.example.lesson_14
 
-open class Chat(
+class Chat(
     val messageList: MutableList<Message> = mutableListOf(),
     val childMessageList: MutableList<ChildMessage> = mutableListOf(),
 ){
-    fun addMessage( name: String, message: String){
+    fun addMessage( userMessage: String, userName: String){
+        val newMessage = Message(
+            id = messageList.size,
+            message = userMessage,
+            name = userName,
+        )
+        messageList.add(newMessage)
     }
-
-    fun addThreadMessage(name: String?, message: String?, parentMessageId: String? = null) {
+    fun addThreadMessage(userMessage: String, userName: String, userParentMessageId: Int) {
+        val threadMessage = ChildMessage(
+            id = messageList.size,
+            message = userMessage,
+            name = userName,
+            parentMessageId = userParentMessageId,
+        )
+        childMessageList.add(threadMessage)
     }
 
     fun printChat(){
-        if()
+        val groupedChildMessage: Map<Int, List<ChildMessage>> = childMessageList.groupBy { it.parentMessageId }
+        messageList.forEach{baseMessage->
+        println("${baseMessage.name}: ${baseMessage.message}")
+            groupedChildMessage[baseMessage.id]?.forEach{childMessage ->
+                println("\t${childMessage.name}: ${childMessage.message}")
+                    }
+    }
     }
 
-}
 open class Message(
     val id: Int,
     val message: String,
@@ -25,4 +42,20 @@ class ChildMessage(
      id: Int,
      message: String,
      name: String,
-): Message(id, message, name){}
+    val parentMessageId: Int,
+): Message(id, message, name)
+}
+
+fun main() {
+    val chat = Chat()
+    chat.addMessage("привет ребята будете в доту играть?", "Артур",)
+    chat.addThreadMessage("неет, у меня сегодня много домашки", "Данила кучерявый", chat.messageList[0].id)
+    chat.addThreadMessage("го к семи освобожусь", "рыжий подбитый жизей", chat.messageList[0].id)
+    chat.addThreadMessage("ребята задрали играть у завтра конрольная", "славик числавик", chat.messageList[0].id)
+    chat.addMessage("ребят ну вы чо?..раз живем чего же", "Артур")
+    chat.addMessage("вово", "вова волк")
+    chat.addThreadMessage("дегенераты", "славик числавик", chat.messageList[1].id)
+    chat.printChat()
+}
+
+
