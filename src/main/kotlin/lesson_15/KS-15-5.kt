7bfc389f6927f735843978e035111cd2b9dae8eb
david <dave.haris06@gmail.com>
2024-10-30 11:00:09 +0300

    KS-15-5 refactored

--- src/main/kotlin/lesson_15/KS-15-5.kt
+++ src/main/kotlin/lesson_15/KS-15-5.kt
@@ -4,30 +4,75 @@ package org.example.lesson_15
 class Trucks(): Moveable{
     val maxPassengerQuantity = 1
     val maxLoadWeight = 2
-
+// логика грузовых автомобилей
     override fun Load(passengersQuantity: Int, loadWeight: Int) {
-        if (passengersQuantity != 0)
-            println("грузовой автомобиль перевозит $maxPassengerQuantity пассажиров" )
-        println("грузовой автомобиль перевозит $maxLoadWeight тон груза" )
+        val weightDifference = loadWeight - maxLoadWeight
+        val passangersDifference = passengersQuantity - maxPassengerQuantity
+        if (( passengersQuantity > 1) and ( loadWeight > 2)){
+            println("грузовой автомобиль перевозит $maxPassengerQuantity пассажиров и $maxLoadWeight тон груза" )
+            println("осталось ${passangersDifference} пассажиров" +
+                    " и ${weightDifference} тон груза")
+        }
+       else if (( passengersQuantity == 1) and ( loadWeight > 2)){
+            println("грузовой автомобиль перевозит $maxPassengerQuantity пассажиров и $maxLoadWeight тон груза" )
+            println("всех пассажир перевезли осталось ${weightDifference} тон груза")
+        }
+       else if((passengersQuantity == 0) and (loadWeight > 2 )){
+            println("грузовой автомобиль перевозит $maxLoadWeight тон груза")
+            println("всех пассажиров перевезли, осталось ${weightDifference} тон груза")
+        }
+        else if((passengersQuantity == 0) and (loadWeight == 2)){
+            println("грузовой автомобиль перевозит $maxLoadWeight тон груза")
+            println("все пассажиры и груз перевезли")
+        }
+        else if ((passengersQuantity == 0) and (loadWeight == 1)){
+            println("грузовой автомобиль перевозит 1 тон груза")
+            println("всех пассажир и груз перевезли")
+        }
+        else{
+            println("всех пассажир и груз перевезли")
+        }
     }
 }
 
 class PassengerCars(): Moveable{
     val maxPassengerQuantity = 3
+    //логика легковых автомобилей
     override fun Load(passengersQuantity: Int, loadWeight: Int) {
-        if (passengersQuantity != 1)
-            println("грузовой автомобиль перевозит $maxPassengerQuantity пассажиров" )
-        else println("грузовой автомобиль перевозит 1 пассажира")
+        val passangersDifference = passengersQuantity - maxPassengerQuantity
+        if ((passengersQuantity > 3) and (loadWeight != 0)){
+            println("легковой автомобиль перевозит $maxPassengerQuantity пассажиров" )
+            println("осталось ${passangersDifference} пассажиров и $loadWeight тон груза")
+        }
+        else if ((passengersQuantity == 3) and (loadWeight != 0)){
+            println("легковой автомобиль перевозит 2 пассажир")
+            println("всех пассажиров перевезли, осталось $loadWeight  тон груза")
+        }
+        else if ((passengersQuantity == 2) and (loadWeight != 0)){
+            println("легковой автомобиль перевозит 2 пассажир")
+            println("всех пассажиров перевезли, осталось $loadWeight  тон груза")
+        }
+        else if ((passengersQuantity == 1) and (loadWeight != 0)){
+            println("легковой автомобиль перевозит 1 пассажир")
+            println("всех пассажиров перевезли, осталось $loadWeight  тон груза")
+        }
+        else if ((passengersQuantity == 0) and (loadWeight != 0)){
+            println("осталось $loadWeight тон груза")
+        }
+        else{println("всех пассажир и груз перевезли")}
     }
 }
 
 interface Moveable { fun Load(passengersQuantity: Int, loadWeight: Int, ) }
+
 fun main(){
     val truck = Trucks()
     val passengerCar = PassengerCars()
 
     passengerCar.Load(6,2)
+    println()
     passengerCar.Load(3,2)
+    println()
     truck.Load(0, 2)
 
 }
