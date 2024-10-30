2bd15da1890fdefdc742e6e3fcc0d6f9298906bd
david <dave.haris06@gmail.com>
2024-10-30 21:33:14 +0300

    KS-16-5 unfinished

--- src/main/kotlin/lesson_16/KS-16-5.kt
+++ src/main/kotlin/lesson_16/KS-16-5.kt
@@ -1,24 +1,53 @@
 package org.example.lesson_16
 
-import kotlin.random.Random
+class Player(val name: String, ){
+    private var health = 10
+    var punchDamageScore = 5
 
-class Player(
-    val name: String,
-){
-    val punchDamageScore = Random.nextInt(1,10)
-    private var health = Random.nextInt(1,10)
+    fun Health(){
+        val player = Player("")
 
-    fun Punch(){
-        println("игрок нанес урон")
+        println("здоровье игрока - ${player.health}")
     }
-    fun Healing(){
-        println("игрок залечил немного раны")
-        health += 1
-    }
-    fun Damage(){
-        println("игрока ранили")
-        health -= 1
+    private fun Death(){
+        val player = Player(" ")
+        println("игрока ${player.name} убили")
+        health = 0
+        punchDamageScore = 0
     }
 
+    fun Punch(punchPoints: Int): Int {
+        val player = Player(" ")
 
+        return punchPoints
+        println("игрок ${player.name} нанес $punchPoints урона")
+    }
+
+    fun Healing(healingPoints: Int){
+        val player = Player(" ")
+
+        if (health != 0){
+            health += healingPoints
+            println("игрок ${player.name} залечил немного раны - ${health}")}
+        else println("игроку ${player.name} умер, лечения не поможет")
+    }
+
+    fun TakingDamage(damagePoints: Int){
+        val player = Player(" ")
+
+        if (player.health > damagePoints){
+            player.health -= damagePoints
+            println("игроку ${player.name} нанесли $damagePoints урона - ${player.health}")
+        }
+        else if (player.health == damagePoints){player.Death()}
+        else println("игрок ${player.name} уже умер")
+    }
+}
+fun main(){
+    val player = Player("Венгер восьмой")
+
+    player.Health()
+    player.TakingDamage(1)
+    player.Health()
+    player.Healing(3)
 }
