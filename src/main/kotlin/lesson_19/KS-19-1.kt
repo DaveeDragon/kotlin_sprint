24650b45e81edf7c8c4c8d370d75d1f7a69993de
david <dave.haris06@gmail.com>
2024-11-02 18:23:00 +0300

    KS-19-1 refactored

--- src/main/kotlin/lesson_19/KS-19-1.kt
+++ src/main/kotlin/lesson_19/KS-19-1.kt
@@ -1,31 +1,17 @@
 package org.example.lesson_19
 
-enum class Fish(val id: Int){
-    GUPPY(1),
-    ANGELFISH(2),
-    GOLDFISH(3),
-    SIAMESE_FIGHTING_FISH(4),
-}
-fun  showFishes(fish: Fish){
-    when(fish){
-        Fish.GUPPY -> println("Гуппи")
-        Fish.ANGELFISH -> println("Скалярий")
-        Fish.GOLDFISH -> println("Золотая рыбка")
-        Fish.SIAMESE_FIGHTING_FISH -> println("Петушок")
-    }
+enum class Fish(val nameFish: String){
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярий"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main(){
      println("В аквариум вы можете добавить таких рыб как:")
-     val listOfFish = listOf(1, 2, 3, 4)
+     val listOfFish = listOf(Fish.GUPPY, Fish.ANGELFISH,Fish.GOLDFISH,Fish.SIAMESE_FIGHTING_FISH)
     for (i in listOfFish){
-        when(i){
-            Fish.GUPPY.id -> showFishes(Fish.GUPPY)
-            Fish.ANGELFISH.id  -> showFishes(Fish.ANGELFISH)
-            Fish.GOLDFISH.id -> showFishes(Fish.GOLDFISH)
-            Fish.SIAMESE_FIGHTING_FISH.id  -> showFishes(Fish.SIAMESE_FIGHTING_FISH)
-        }
-        Thread.sleep(1000)
+        println(i.nameFish)
     }
 }
 
