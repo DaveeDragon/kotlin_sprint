32f249d3df7fa7b04041e6d44267ded8ccbff9a7
david <dave.haris06@gmail.com>
2024-11-02 19:46:46 +0300

    KS-17-3 refactored

--- src/main/kotlin/lesson_17/KS-17-3.kt
+++ src/main/kotlin/lesson_17/KS-17-3.kt
@@ -3,12 +3,20 @@ package org.example.lesson_17
 class Folder(
     val secretFlag: Boolean,
 ){
-    val filesQuantity = 32
+    val filesQuantity: Int = 32
+        get()  = if (secretFlag == true) 0
+        else field
+
     val name: String = "Топ сикрет"
-        get()  = if (secretFlag == true) "скрытая папка,количество файлов - 0"
-        else " папка $field, количество файлов - $filesQuantity"
+        get()  = if (secretFlag == true)"скрытая папка"
+        else "папка $field"
 }
 fun main(){
     val folder = Folder(true)
     println(folder.name)
+    println(folder.filesQuantity)
+    println()
+    val folder1 = Folder(false)
+    println(folder1.name)
+    println(folder1.filesQuantity)
 }
