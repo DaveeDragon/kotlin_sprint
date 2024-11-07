e61ea57bd744b682fe95b05db9b03e0766939f47
david <dave.haris06@gmail.com>
2024-11-08 02:55:33 +0300

    KS-19-4 refactored2

--- src/main/kotlin/lesson_19/KS-19-4.kt
+++ src/main/kotlin/lesson_19/KS-19-4.kt
@@ -1,25 +1,23 @@
 package org.example.lesson_19
 
-enum class Bullets(var damage: Int) {
-    NOTHING(0),
+enum class Bullet(var damage: Int?) {
     BLUE(5),
     GREEN(10),
     RED(20);
 }
 
-class Tank(){
-    var bullet = Bullets.NOTHING
-    fun bulletChange(bulletName: Bullets) {
+class Tank(private var bullet: Bullet? = null){
+    fun bulletChange(bulletName: Bullet) {
         when(bulletName){
-            Bullets.BLUE -> bullet = Bullets.BLUE
-            Bullets.GREEN -> bullet = Bullets.GREEN
-            Bullets.RED -> bullet = Bullets.RED
-            else -> bullet = Bullets.NOTHING
+            Bullet.BLUE -> bullet= Bullet.BLUE
+            Bullet.GREEN -> bullet = Bullet.GREEN
+            Bullet.RED -> bullet = Bullet.RED
         }
     }
     fun shot() {
-        if (bullet.damage > 0)
-            println("вы нанесли ${bullet.damage} урона")
+        var isBulletNull = bullet == null
+        if (!isBulletNull)
+            println("вы нанесли ${bullet?.damage} урона")
         else println("танк не заряжан")
     }
 }
@@ -27,10 +25,10 @@ fun main() {
     val tank1 = Tank()
     tank1.shot()
     println()
-    tank1.bulletChange(Bullets.BLUE)
+    tank1.bulletChange(Bullet.BLUE)
     tank1.shot()
     println()
-    tank1.bulletChange(Bullets.RED)
+    tank1.bulletChange(Bullet.RED)
     tank1.shot()
 
 }
