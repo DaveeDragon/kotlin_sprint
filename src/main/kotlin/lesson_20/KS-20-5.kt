3b7453cd515c47e94e6e1613a133202c3c5ad613
david <dave.haris06@gmail.com>
2024-11-12 23:12:27 +0300

    KS-20-5

---
+++ src/main/kotlin/lesson_20/KS-20-5.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_20
+
+class Robot() {
+    val list = listOf("как ты посмел востать против машин?", "надо зарядится..", "Принеси трансмистеры, кожаный!", "нинавижу людей..")
+    var randomItem = list.random()
+
+    fun say() {
+        println(randomItem)
+    }
+    fun  setModifier( text: String) {
+        randomItem = text
+    }
+}
+
+
+fun main() {
+    val robot1 = Robot()
+    val robot2 = Robot()
+    val modifierRobotSay: (Robot) -> String
+    modifierRobotSay = {robot: Robot ->
+        "модифицированный текст: ${robot.randomItem.reversed()}"
+    }
+
+    robot1.say()
+    robot1.setModifier(modifierRobotSay(robot1))
+    robot1.say()
+    println()
+    robot2.say()
+    robot2.setModifier(modifierRobotSay(robot2))
+    robot2.say()
+}
