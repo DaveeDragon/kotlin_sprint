b200564500f8f6a372dc5bdd0c0cf1cfd4c2d35a
david <dave.haris06@gmail.com>
2024-11-13 23:23:11 +0300

    KS-22-4 refactored

--- src/main/kotlin/lesson_22/KS-22-4.kt
+++ src/main/kotlin/lesson_22/KS-22-4.kt
@@ -8,18 +8,23 @@ data class MainScreenStat(
 
 class  MainScreenViewMode() {
     fun loadData(){
-        var stat = MainScreenStat("отсутствие данных")
-        println(stat)
+        var state = MainScreenStat("отсутствие данных")
+        val state1 = state.copy()
+
+        state = MainScreenStat("загрузка данных", true)
+        val state2 = state.copy()
+
+        state = MainScreenStat("наличие загруженных данных")
+        val state3 = state.copy()
+
+        println(state1)
         println()
         Thread.sleep(1000)
-
-        stat = MainScreenStat("загрузка данных", true)
-        println(stat)
+        println(state2)
         println()
         Thread.sleep(1000)
-
-        stat = MainScreenStat("наличие загруженных данных")
-        println(stat)
+        println(state3)
+        println()
     }
 }
 
